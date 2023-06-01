3437dc1c3223ea05fd08c6ca291bfe57edbebfea
luklukass <104025385+luklukass@users.noreply.github.com>
2023-06-01 15:34:36 +0200

    1st version

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,184 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Earnings Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: EarningsPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class EarningsPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _EarningsPageState createState() => _EarningsPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _EarningsPageState extends State<EarningsPage> {
+   DateTime? _startDate;
+   DateTime? _endDate;
+  double _earnedMoney = 0.0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+   void _calculateEarnings() {
+     if (_startDate == null || _endDate == null || _earnedMoney <= 0) {
+       return;
+     }
+
+     DateTime now = DateTime.now();
+     if (now.isBefore(_startDate!) || now.isAfter(_endDate!.add(Duration(days: 1)))) {
+       showDialog(
+         context: context,
+         builder: (BuildContext context) {
+           return AlertDialog(
+             title: Text('Invalid Date'),
+             content: Text('Please enter a valid date range.'),
+             actions: [
+               TextButton(
+                 child: Text('OK'),
+                 onPressed: () {
+                   Navigator.of(context).pop();
+                 },
+               ),
+             ],
+           );
+         },
+       );
+       return;
+     }
+
+     int daysPassed = now.difference(_startDate!).inDays + 1;
+     double earningsForWholePeriod = _earnedMoney * daysPassed;
+     double earningsFromDateToCurrent = _earnedMoney * now.difference(_startDate!).inDays;
+
+     showDialog(
+       context: context,
+       builder: (BuildContext context) {
+         return AlertDialog(
+           title: Text('Earnings Calculation'),
+           content: Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+             mainAxisSize: MainAxisSize.min,
+             children: [
+               Text('Earnings for the whole period: \$${earningsForWholePeriod.toStringAsFixed(2)}'),
+               SizedBox(height: 10),
+               Text('Earnings from start date to current date: \$${earningsFromDateToCurrent.toStringAsFixed(2)}'),
+             ],
+           ),
+           actions: [
+             TextButton(
+               child: Text('OK'),
+               onPressed: () {
+                 Navigator.of(context).pop();
+               },
+             ),
+           ],
+         );
+       },
+     );
+   }
+
+  Future<void> _selectDate(BuildContext context, bool isStartDate) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null) {
+      setState(() {
+        if (isStartDate) {
+          _startDate = picked;
+        } else {
+          _endDate = picked;
+        }
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Earnings Tracker'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Set Start and End Dates',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Text('Start Date'),
+                    SizedBox(height: 10.0),
+                    Text(_startDate == null ? 'Not set' : DateFormat('yyyy-MM-dd').format(_startDate!)),
+                    SizedBox(height: 10.0),
+                    ElevatedButton(
+                      child: Text('Select'),
+                      onPressed: () {
+                        _selectDate(context, true);
+                      },
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('End Date'),
+                    SizedBox(height: 10.0),
+                    Text(_endDate == null ? 'Not set' : DateFormat('yyyy-MM-dd').format(_endDate!)),
+                    SizedBox(height: 10.0),
+                    ElevatedButton(
+                      child: Text('Select'),
+                      onPressed: () {
+                        _selectDate(context, false);
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Enter Earned Money for One Day',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  _earnedMoney = double.parse(value);
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Earned Money',
+                hintText: 'Enter amount',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              child: Text('Calculate'),
+              onPressed: _calculateEarnings,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
