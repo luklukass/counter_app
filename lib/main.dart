e80a4f6df2a2e21d0964ec36fd8044a929007cd5
luklukass <104025385+luklukass@users.noreply.github.com>
2023-06-04 11:42:21 +0200

    5st version, czech language

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-    runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -17,16 +16,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Earnings Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      // Apply dark theme to the app
       home: EarningsPage(),
-      localizationsDelegates: const [
+      localizationsDelegates: [
         GlobalMaterialLocalizations.delegate
       ],
-      supportedLocales: const [
+      supportedLocales: [
         Locale('en'),
         Locale('cs')
       ],
@@ -57,8 +54,8 @@ class _EarningsPageState extends State<EarningsPage> {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: const Text('Invalid Date'),
-            content: const Text('Please enter a valid date range.'),
+            title: const Text('Špatné datum'),
+            content: const Text('Datum příjezdu nesmí být v budoucnu.'),
             actions: [
               TextButton(
                 child: const Text('OK'),
@@ -118,7 +115,12 @@ class _EarningsPageState extends State<EarningsPage> {
       initialDate: DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2100),
-
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.dark(), // Apply dark theme to the date picker
+          child: child!,
+        );
+      },
     );
 
     if (picked != null) {
@@ -137,74 +139,126 @@ class _EarningsPageState extends State<EarningsPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Motivační kalkulačka'),
+        backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Nastav datum přijezdu a odjezdu',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: [
-                    const Text('Příjezd'),
-                    const SizedBox(height: 10.0),
-                    Text(_startDate == null ? 'Nevybráno' : DateFormat.yMd('cs').format(_startDate!)),
-                    const SizedBox(height: 10.0),
-                    ElevatedButton(
-                      child: const Text('Vyber'),
-                      onPressed: () {
-                        _selectDate(context, true);
-                      },
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    const Text('Odjezd'),
-                    const SizedBox(height: 10.0),
-                    Text(_endDate == null ? 'Nevybráno' : DateFormat.yMd('cs').format(_endDate!)),
-                    const SizedBox(height: 10.0),
-                    ElevatedButton(
-                      child: const Text('Vyber'),
-                      onPressed: () {
-
-                        _selectDate(context, false);
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Zadej částku výdělku za jeden den',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 10.0),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _earnedMoney = double.parse(value);
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Částka',
-                hintText: 'Zadej částku',
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black87,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      const Text(
+                        'Příjezd',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.yellowAccent,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Text(
+                        _startDate == null ? 'Nevybráno' : DateFormat.yMd('cs').format(_startDate!),
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          color: Colors.yellowAccent,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      ElevatedButton(
+                        child: const Text('Vyber'),
+                        onPressed: () {
+                          _selectDate(context, true);
+                        },
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text(
+                        'Odjezd',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.greenAccent,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Text(
+                        _endDate == null ? 'Nevybráno' : DateFormat.yMd('cs').format(_endDate!),
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          color: Colors.greenAccent,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      ElevatedButton(
+                        child: const Text('Vyber'),
+                        onPressed: () {
+                          _selectDate(context, false);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _calculateEarnings,
-              child: const Text('Vypočítej'),
-            ),
-          ],
+              const SizedBox(height: 20.0),
+              const Text(
+                'Zadej částku výdělku za jeden den',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              TextField(
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                keyboardAppearance: Brightness.dark,
+                onChanged: (value) {
+                  setState(() {
+                    _earnedMoney = double.parse(value);
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Částka',
+                  hintText: 'Zadej částku',
+                  labelStyle: TextStyle(
+                    color: Colors.red,
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.green,
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+                width: 100,
+              ),
+              ElevatedButton(
+                onPressed: _calculateEarnings,
+                child: const Text('Vypočítej'),
+              ),
+            ],
+          ),
         ),
       ),
     );
